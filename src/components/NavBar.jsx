36fedcb25c1441afe1e824136a1a5027d91c4998
Cami0703 <camiladiazcaceres028@gmail.com>
2024-10-28 15:05:17 -0300

    ordenando codigo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import '../index.css';
 import React, { useState } from "react";
 import logoCoffecode from '../assets/logo.svg';
@@ -19,7 +19,7 @@ export default function NavBar() {
   };
 
 
-  
+
 
   return (
     <>
@@ -29,89 +29,89 @@ export default function NavBar() {
           DashBoard
         </h1>
         <div className="nav-item active">
-        <NavLink className="nav-link" to="/">
+          <NavLink className="nav-link" to="/">
             <img className="logocfc" src={logoCoffecode} alt="logo" />
           </NavLink>
           <form method="post">
-            <button style={{color:"#823a6f"}} type="submit">Nuevo Producto</button>
+            <button style={{ color: "#823a6f" }} type="submit">Nuevo Producto</button>
           </form>
         </div>
         <nav>
-        <span className="span2">CoffeCode -- DashBoard</span>
-        <hr />
-        <div className="drop-panel">
-          <button onClick={toggleDropdown} className="drop-btn" >
-            Total
-          </button>
-          {isDropdownOpen && (
-            <ul className="menu-drop">
-              <li>
-              <NavLink 
-                  to="/total-products" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Total de Productos</NavLink>
-              </li>
-              <li>
-              <NavLink 
-                  to="/total-users" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Total de Usuarios</NavLink>
-              </li>
-              <li>
-              <NavLink 
-                  to="/total-categories" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Total de Categorías</NavLink>
-              </li>
-            </ul>
-          )}
-        </div>
-        <div className="drop-panel">
-          <button onClick={toggleSalesDropdown} className="drop-btn">
-            Ventas
-          </button>
-
-          {isSalesDropdownOpen && (
-            <ul className="drop-menu">
-              <li>
-              <NavLink 
-                  to="/total-products-sold" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Total de Productos Vendidos</NavLink>
-              </li>
-              <li>
-              <NavLink 
-                  to="/total-sales" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Total de Ventas</NavLink>
-              </li>
-              <li>
-              <NavLink 
-                  to="/last-5-sales" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Últimos 5 Productos Vendidos</NavLink>
-              </li>
-              <li>
-              <NavLink 
-                  to="/top-5-sales" 
-                  className={({ isActive }) => isActive ? "active" : ""}
-                >Top 5 Productos Más Vendidos</NavLink>
-              </li>
-            </ul>
-          )}
-        </div>
-        <li className="nav-item">
-        <NavLink 
-            className={({ isActive }) => isActive ? "active" : "nav-link"} 
-            to="/last-product-in-db"
-          >
-                        <i className="fas fa-coffee"></i>
-                        <p className="span1">Detalle ultimo Producto en BD</p></NavLink>
+          <span className="span2">CoffeCode -- DashBoard</span>
+          <hr />
+          <div className="drop-panel">
+            <button onClick={toggleDropdown} className="drop-btn" >
+              Total
+            </button>
+            {isDropdownOpen && (
+              <ul className="menu-drop">
+                <li>
+                  <NavLink
+                    to="/total-products"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Total de Productos</NavLink>
                 </li>
+                <li>
+                  <NavLink
+                    to="/total-users"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Total de Usuarios</NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/total-categories"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Total de Categorías</NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+          <div className="drop-panel">
+            <button onClick={toggleSalesDropdown} className="drop-btn">
+              Ventas
+            </button>
+
+            {isSalesDropdownOpen && (
+              <ul className="drop-menu">
+                <li>
+                  <NavLink
+                    to="/total-products-sold"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Total de Productos Vendidos</NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/total-sales"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Total de Ventas</NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/last-5-sales"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Últimos 5 Productos Vendidos</NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/top-5-sales"
+                    className={({ isActive }) => isActive ? "active" : ""}
+                  >Top 5 Productos Más Vendidos</NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => isActive ? "active" : "nav-link"}
+              to="/last-product-in-db"
+            >
+              <i className="fas fa-coffee"></i>
+              <p className="span1">Detalle ultimo Producto en BD</p></NavLink>
+          </li>
         </nav>
       </div>
       <div id="detail">
-          <Outlet />
+        <Outlet />
       </div>
     </>
   );
