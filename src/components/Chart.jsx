6ded196683d5fcdf8cd444e32c7e22bd17a24d89
Cami0703 <camiladiazcaceres028@gmail.com>
2024-11-03 16:04:09 -0300

    agregando nombres de cafe a tabla de productos

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,12 +5,66 @@ import ChartRow from './ChartRow';
 
 let tableRowsData = [
     {
-        Nombre: 'Billy Elliot ',
+        Nombre: 'API AROMA ',
         Presentacion: ['250grs', '1kg'],
         Tipo: '5',
         Categories: ['Drama', 'Comedia'],
         Awards: 2
     },
+    {
+        Nombre: 'ORACLE',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'SCRIPT',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'STACK LATTE',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'SYNTAX LATTE',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'LOGIC',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'FULL STACK',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+    {
+        Nombre: 'EXPRESS',
+        Presentacion: ['250grs', '1kg'],
+        Tipo: '5',
+        Categories: ['Drama', 'Comedia'],
+        Awards: 2
+    },
+
+
+
+
+
 
 ]
 
