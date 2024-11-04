741d303864e1cd053f0542c9f58bec342ef4967b
facudiaz16 <fdiazbordon@gmail.com>
2024-11-03 23:19:34 -0300

    agregando react modal e iframe para ver el formulario de creacion de producto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,50 +1,48 @@
+import React, { useState } from "react";
+import Modal from "react-modal"; 
 import { NavLink, Outlet } from "react-router-dom";
-import '../index.css';
-import { useState } from "react";
 import logoCoffecode from '../assets/logo.svg';
 
+Modal.setAppElement('#root'); 
+
 export default function NavBar() {
-  // Estado para controlar el menú desplegable
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [isSalesDropdownOpen, setSalesDropdownOpen] = useState(false);
-
-
-  // Función para alternar el menú desplegable
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
+  const [isModalOpen, setModalOpen] = useState(false); 
 
   const toggleSalesDropdown = () => {
     setSalesDropdownOpen(!isSalesDropdownOpen);
   };
 
+  const openModal = () => {
+    setModalOpen(true); 
+  };
 
-
+  const closeModal = () => {
+    setModalOpen(false); 
+  };
 
   return (
     <>
       <div id="sidebar">
         <h1 style={{ color: "#823a6f" }}>
-          <img className="logocfc" src={logoCoffecode} alt="logo" style={{width: "250px", height: "80px" }} />
-          
+          <img className="logocfc" src={logoCoffecode} alt="logo" style={{ width: "250px", height: "80px" }} />
         </h1>
         <div className="nav-item active">
           <NavLink className="nav-link" to="/">
-            <img className="logocfc" src={logoCoffecode} alt="logo" style={{width: "250px", height: "80px" }} />
-           <hr /> 
+            <img className="logocfc" src={logoCoffecode} alt="logo" style={{ width: "250px", height: "80px" }} />
+            <hr />
             <span className="span2">COFFEECODE - DASHBOARD</span>
           </NavLink>
-          
         </div>
         <nav>
-        <li className="nav-item">
+          <li className="nav-item">
             <NavLink
               className={({ isActive }) => isActive ? "" : "nav-link"}
               to="/"
             >
-              <i className="fas fa-home" ></i>
+              <i className="fas fa-home"></i>
               <p className="span1">Home</p>
-              </NavLink>
+            </NavLink>
           </li>
           <li className="nav-item">
             <NavLink
@@ -52,7 +50,8 @@ export default function NavBar() {
               to="/last-product-in-db"
             >
               <i className="fas fa-coffee"></i>
-              <p className="span1">Detalle ultimo Producto en BD</p></NavLink>
+              <p className="span1">Detalle último Producto en BD</p>
+            </NavLink>
           </li>
           <li className="nav-item">
             <NavLink
@@ -60,30 +59,32 @@ export default function NavBar() {
               to="/total"
             >
               <i className="fas fa-box"></i>
-              <p className="span1">Total</p></NavLink>
+              <p className="span1">Total</p>
+            </NavLink>
           </li>
           <li className="nav-item">
             <NavLink
               className={({ isActive }) => isActive ? "active" : "nav-link"}
               to="/tipos-cafes">
               <i className="fas fa-coffee"></i>
-              <p className="span1">Tipos de cafes</p></NavLink>
+              <p className="span1">Tipos de cafés</p>
+            </NavLink>
           </li>
           <li className="nav-item">
             <NavLink
               className={({ isActive }) => isActive ? "active" : "nav-link"}
               to="/lista-productos">
               <i className="fas fa-coffee"></i>
-              <p className="span1">Lista de productos</p></NavLink>
+              <p className="span1">Lista de productos</p>
+            </NavLink>
           </li>
-          <form method="post">
-            <button className="drop-btn" type="submit">+ Nuevo Producto</button>
-          </form>
+          <button onClick={openModal} className="drop-btn">
+            + Nuevo Producto
+          </button>
           <div className="drop-panel">
             <button onClick={toggleSalesDropdown} className="drop-btn">
               Ventas
             </button>
-
             {isSalesDropdownOpen && (
               <ul className="drop-menu">
                 <li>
@@ -115,8 +116,27 @@ export default function NavBar() {
           </div>
         </nav>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Formulario de Creación de Producto"
+        className="Modal" 
+        overlayClassName="Overlay" 
+      >
+        
+        <iframe
+          src="http://localhost:3000/admin/create" 
+          style={{
+            width: "100%",
+            height: "90vh",
+            border: "none",
+          }}
+          title="Formulario de creación de producto"
+        ></iframe>
+        <button onClick={closeModal}>Cerrar</button>
+      </Modal>
       <div id="detail">
-        <Outlet />
+        <Outlet /> 
       </div>
     </>
   );
